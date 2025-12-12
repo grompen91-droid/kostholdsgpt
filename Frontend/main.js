4b44c1e642c6e748ba5919c8276d2aef7fbb25ba
nox <nox.heights@gmail.com>
2025-12-12 13:43:18 +0100

    Første push, satt opp backend med hjelp fra claude, kan nå lage Frontend og lage funksjonalitet for å chatte med ai agent. For å bruke tjenesten må du hoste backend. Henvend deg til  README.

---
+++ Frontend/main.js
@@ -0,0 +1,16 @@
+fetch(`http://localhost:3000/api/chat`, {
+    method: `POST`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        model: `gpt-oss:120b-cloud`,
+        messages: [
+            {
+                role: `user`,
+                content: `Who are you?`
+            }
+        ]
+    })
+}).then(data => console.log(data.json()))
+.catch(error => console.error(error));

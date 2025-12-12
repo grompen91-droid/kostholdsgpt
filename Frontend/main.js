2a4aa5d5189078183dff446c1fe3b3a80221c4b1
nox <nox.heights@gmail.com>
2025-12-12 14:29:18 +0100

    kommenterte ut alt

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,16 +1,18 @@
-fetch(`http://localhost:3000/api/chat`, {
-    method: `POST`,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-        model: `gpt-oss:120b-cloud`,
-        messages: [
-            {
-                role: `user`,
-                content: `Who are you?`
-            }
-        ]
-    })
-}).then(data => console.log(data.json()))
-.catch(error => console.error(error));
+
+
+// fetch(`http://localhost:3000/api/chat`, {
+//     method: `POST`,
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//         model: `gpt-oss:120b-cloud`,
+//         messages: [
+//             {
+//                 role: `user`,
+//                 content: `Who are you?`
+//             }
+//         ]
+//     })
+// }).then(data => console.log(data.json()))
+// .catch(error => console.error(error));

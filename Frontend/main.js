5664f9c232746ab7d6a86821872147f193dc9dbd
nox <nox.heights@gmail.com>
2025-12-16 15:24:36 +0100

    fix

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,4 +1,22 @@
 
+async function loadPrompt() {
+    try {
+        const response = await fetch(`./prompt.json`)
+        return await response.json();
+    }   catch (error){
+        console.error(`Noe gikk feil ${error.message}`)
+    }
+}
+const jsonTemplate = loadPrompt();
+console.log(jsonTemplate);
+
+const input = document.getElementById(`input`);
+
+/* input.addEventListener(`click`, (e) => {
+    if (e.key === `Enter`) {
+        console.log(`${e.key}, key pressed! ${jsonTemplate}`)
+    }});*/
+
 
 // fetch(`http://localhost:3000/api/chat`, {
 //     method: `POST`,
